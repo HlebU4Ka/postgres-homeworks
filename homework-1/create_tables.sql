20dba27dee0eebfecac188b398256a3dfbccad7b
glebk <glebkot57@gmail.com>
2023-07-13 06:48:58 +0200

    создал файлы

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
+CREATE TABLE customers_data
+(
+    customer_id varchar(10) PRIMARY KEY,
+    company_name varchar(50) NOT NULL,
+    contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE employees_data
+(
+    employee_id int PRIMARY KEY,
+    first_name varchar(10) NOT NULL,
+    last_name varchar(20) NOT NULL,
+    title text NOT NULL,
+    birth_date date NOT NULL,
+    note text
+);
+
+CREATE TABLE orders_data
+(
+    order_id int PRIMARY KEY,
+    customer_id varchar(10) REFERENCES customers_data(customer_id) NOT NULL,
+    employee_id int NOT NULL,
+    order_date date NOT NULL,
+    ship_city varchar(50) NOT NULL
+);
+
 -- SQL-команды для создания таблиц
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,24 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+import os
+
+USER = "glebk"
+PASSWORD = "PASSWORD"
+
+user = ('USER')
+password = ('PASSWORD')
+
+with psycopg2.connect(host="localhost",
+                      database="postgres-homeworks",
+                      user=user,
+                      password="0052533") as con:
+    try:
+        with con.cursor() as cur:
+                with open(os.path.join('..', 'homework-1', 'postgres-homeworks', 'customers_data.csv'), 'r',
+                          encoding='utf-8') as f:
+                    csv_text = csv.reader(f)
+                    next(csv_text)
+                    for i in csv_text:
+                        cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", tuple(i))
+    except psycopg2.Error as e:
+        print("Error connecting to the database:", e)
